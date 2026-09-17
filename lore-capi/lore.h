57e1fc1135e23edf4eb8332735832e8c515a8755
Mattias Jansson <mattias.jansson@epicgames.com>
2026-09-17 12:21:42 +0000

    lore: Bump nightly version
    Lore-RevId: 1177
    Lore-Signature: 0b3cb788bdb8de2be3e4560fbc2034d24b559c3862a961ded94b455fa65e4ff2

--- lore-capi/lore.h
+++ lore-capi/lore.h
@@ -50,7 +50,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define LORE_INTERFACE_VERSION "0.10.0-nightly"
+#define LORE_INTERFACE_VERSION "0.10.1-nightly"
 
 // The kind of value held by a metadata entry.
 //
